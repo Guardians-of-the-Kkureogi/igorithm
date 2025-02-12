7897f5bd5e718aedb8b391f2f690cfdf8471c90b
지수민 <wltn1873@gmail.com>
2025-02-12 22:31:56 +0900

    Solved : Programmers 2 x n 타일링

---
+++ 0212/Programmers_2 x n 타일링_수민.js
@@ -0,0 +1,10 @@
+// dp[n] = dp[n-1] + dp[n-2]
+
+function solution(n) {
+  const dp = [0, 1, 2];
+
+  for (let i = 3; i < n + 1; i++) {
+    dp.push((dp[i - 1] + dp[i - 2]) % 1000000007);
+  }
+  return dp[n];
+}
